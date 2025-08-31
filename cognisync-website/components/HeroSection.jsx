aa0f1591fce0965dff6f2b88d57b6cb872e880c6
chrisb1005 <chrisbattle56@gmail.com>
2025-08-31 09:31:47 -0500

    updated button css again x7

--- cognisync-website/components/HeroSection.jsx
+++ cognisync-website/components/HeroSection.jsx
@@ -9,10 +9,30 @@ const HeroSection = () => {
       <img src={logoImage} alt="CogniSync Logo" className="logo" />
       <div className='button-container'>
       <a href="#services-section">
-      <button className='cb-header-services-button'>Our Services</button>
+      <button className='cb-header-services-button' style={{
+         background: "#1f2833",
+    color: "#ffffff",
+    border: "2px solid #66fcf1",
+    padding: "10px 20px",
+    borderRadius: "8px",
+    fontSize: "1rem",
+    cursor: "pointer",
+    display: "inline-block",
+    whiteSpace: "nowrap",
+      }}>Our Services</button>
       </a>
       <a href="#contact-section">
-          <button className="cb-header-signup-button">Lets Chat!</button>
+          <button className="cb-header-signup-button"style={{
+             background: "#1f2833",
+    color: "#ffffff",
+    border: "2px solid #66fcf1",
+    padding: "10px 20px",
+    borderRadius: "8px",
+    fontSize: "1rem",
+    cursor: "pointer",
+    display: "inline-block",
+    whiteSpace: "nowrap",
+          }}>Lets Chat!</button>
           </a>
       </div>
       
@@ -23,10 +43,30 @@ const HeroSection = () => {
         <h3>Empowering Efficiency: Connect your tools. Automate your work. Focus on growth.</h3>
         <div className="hero-buttons">
         <a href="#services-section">
-          <button className="cb-header-services-button">Our Services</button>
+          <button className="cb-header-services-button" style={{
+             background: "#1f2833",
+    color: "#ffffff",
+    border: "2px solid #66fcf1",
+    padding: "10px 20px",
+    borderRadius: "8px",
+    fontSize: "1rem",
+    cursor: "pointer",
+    display: "inline-block",
+    whiteSpace: "nowrap",
+          }}>Our Services</button>
           </a>
           <a href="#contact-section">
-          <button className="cb-header-signup-button">Lets Chat!</button>
+          <button className="cb-header-signup-button"style={{
+             background: "#1f2833",
+    color: "#ffffff",
+    border: "2px solid #66fcf1",
+    padding: "10px 20px",
+    borderRadius: "8px",
+    fontSize: "1rem",
+    cursor: "pointer",
+    display: "inline-block",
+    whiteSpace: "nowrap",
+          }}>Lets Chat!</button>
           </a>
         </div>
       </div>
