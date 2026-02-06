28d8769fdaed7a0c0f04bcc51b0c64d8349b5cc7
Christopher Battle <70242434+chrisb1005@users.noreply.github.com>
2026-02-06 15:33:52 -0600

    changed hero section background

--- cognisync-website/components/App.jsx
+++ cognisync-website/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './App.module.css';
 import HeroSection from './HeroSection';
 import AboutUsSection from './AboutUsSection';
@@ -6,20 +6,10 @@ import ServicesSection from './ServicesSection';
 import ContactForm from './ContactForm';
 import Mission from './MissionStatement';
 import Footer from './Footer';
-import TRUNK from 'vanta/src/vanta.dots.js';
 
 const App = () => {
-  useEffect(() => {
-    TRUNK({
-      el: '#vanta',
-      mouseControls: false,
-      touchControls: false,
-    });
-  }, []);
-
   return (
     <div className={styles.App}>
-      <div className={styles.bg} id="vanta"></div>
       <HeroSection />
       <AboutUsSection />
       <Mission />
--- cognisync-website/components/HeroSection.jsx
+++ cognisync-website/components/HeroSection.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './HeroSection.module.css';
 import logoImage from '../logo/CogniSync-logos_white.png';
+import TRUNK from 'vanta/src/vanta.globe';
 
 const HeroSection = () => {
+  useEffect(() => {
+    TRUNK({
+      el: '#hero-vanta',
+      mouseControls: false,
+      touchControls: false,
+      scale: 1,
+      minHeight: 650,
+      minWidth: 300,
+      backgroundColor: 0x1a1a1a,
+      color: 0x404040,
+    });
+  }, []);
+
   return (
     <div className={styles.heroSection}>
+      <div className={styles.vantaBg} id="hero-vanta"></div>
       <img src={logoImage} alt="CogniSync Logo" className={styles.logo} />
       <div className={styles.buttonContainer}>
         <a href="#services-section">
